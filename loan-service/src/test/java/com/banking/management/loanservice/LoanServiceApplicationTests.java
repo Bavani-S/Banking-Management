d56cff33ac0352cae6e52b02e1698bb5eac4ca1e
VS999B2 <bavani.s@pearson.com>
2023-09-25 12:57:59 +0530

    Initial commit

---
+++ loan-service/src/test/java/com/banking/management/loanservice/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.management.loanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
